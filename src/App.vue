a56d33477110678d86e61752c8e310778ba9cbf2
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-05-22 12:30:46 +0200

    Project setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <HomeView />
+    </div>
+</template>
+
+<script>
+import HomeView from "@/views/HomeView.vue";
+
+export default {
+    name: "App",
+    components: {
+        HomeView
+    }
+}
+</script>
