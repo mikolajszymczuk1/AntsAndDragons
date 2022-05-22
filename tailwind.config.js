8e472826e5211e42111267c9476fdbf7981f9ccc
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-05-22 21:19:53 +0200

    Updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
         "./src/**/*.{vue,js,ts,jsx,tsx}"
     ],
     theme: {
-        extend: {},
+        colors: {
+            "y-in-mn-blue": "#355070ff",
+            "chinese-violet": "#6d597aff",
+            "cinnamon-satin": "#b56576ff",
+            "candy-pink": "#e56b6fff",
+            "tumbleweed": "#eaac8bff"
+        },
+        fontFamily: {
+            "noto": ['Noto Sans', 'sans-serif'],
+            "open": ['Open Sans', 'sans-serif']
+        },
+        screens: {
+            "sm": "800px",
+            "md": "1200px"
+        }
     },
     plugins: [],
 };
