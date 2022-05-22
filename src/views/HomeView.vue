a2de3b014a802a6cd6355cb9ca2e30bcb4f3597a
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-05-22 13:01:42 +0200

    Add TailwindCSS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <h1>Home Page</h1>
+        <h1 class="text-[2em] text-center p-5 text-orange-400 font-bold">
+            Home Page
+        </h1>
     </div>
 </template>
 
