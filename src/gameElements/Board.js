e33c8acc8ae083f52f28617d8851ae2559ec42f7
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-05-25 01:56:55 +0200

    Created placeGameEelement method

--- src/gameElements/Board.js
+++ src/gameElements/Board.js
@@ -1,3 +1,7 @@
+import Ant from "./Ant";
+import Dragon from "./Dragon";
+import Food from "./Food";
+
 // Game board class
 class Board {
     constructor(size) {
@@ -20,6 +24,19 @@ class Board {
     // Return board array
     getBoard() {
         return this.board;
+    }
+
+    // Place on board Ant or Dragon or Food
+    placeGameElement(x, y) {
+        if (this.board[y][x] === " ") {
+            this.board[y][x] = new Ant(100);
+        } else if (this.board[y][x].getClassName() === "Ant") {
+            this.board[y][x] = new Dragon(200);
+        } else if (this.board[y][x].getClassName() === "Dragon") {
+            this.board[y][x] = new Food();
+        } else {
+            this.board[y][x] = " ";
+        }
     }
 }
 
