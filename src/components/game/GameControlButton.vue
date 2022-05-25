cecbe299c60746cb564c01c10700066de7fc0062
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-05-25 15:52:13 +0200

    Created GameControlButton component

---
+++ src/components/game/GameControlButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- Game control button component -->
+    <button class="rounded-[10px] font-open font-semibold flex justify-center items-center"
+        :style="`background-color: ${ bgColor }; color: ${ textColor };`"
+    >
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        name: "GameControlButton",
+        props: {
+            bgColor: {
+                type: String,
+                default: "#355070"
+            },
+            textColor: {
+                type: String,
+                default: "white"
+            }
+        }
+    }
+</script>
