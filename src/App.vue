a696141458750f1c76581ba2de58531ae6651938
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-05-25 15:51:41 +0200

    Add decoration corner

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
     <NavigationBar />
+    
+    <!-- Decoration corner -->
+    <img class="absolute top-0 left-0 sm:w-[120px] md:w-[200px] lg:w-[289px]"
+        src="@/assets/svg/DecorationCorner.svg"
+        alt="Decoration wave corner icon"
+        title="Decoration corner"
+    >
 
     <main>
         <GameView />
