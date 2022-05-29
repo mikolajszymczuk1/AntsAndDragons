1c6daf6c02651ffeb2c7348ef054e51e4a89453a
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-05-29 03:11:31 +0200

    Created basic logic for Ants and Dragons

--- src/gameElements/Ant.js
+++ src/gameElements/Ant.js
@@ -10,9 +10,32 @@ class Ant extends Base {
 
     // Move to next position
     move(board) {
-        let neighbors = this.getNeighbors(board, 1);
-        let randomWay = neighbors[Base.randomNumber(0, neighbors.length - 1)];
+        let neighbors;
+        let randomWay;
+        
+        neighbors = this.getNeighbors(board, 1);
+        if (neighbors.length === 0) return;
+        randomWay = neighbors[Base.randomNumber(0, neighbors.length - 1)];
 
+        // Actions controller
+        for (let i = 0; i < neighbors.length; i++) {
+            if (board.checkCell(neighbors[i]) === "food") {  // If found food, eat it
+                randomWay = neighbors[i];
+
+                if ((this.health + 20) > 100) this.health = 100;
+                else this.health += 20;
+                break;
+
+            } else if (board.checkCell(neighbors[i]) === "dragon" || board.checkCell(neighbors[i]) === "ant") {  // If found ant or dragon, go to free place
+                let filteredNeighbors = neighbors.filter((el) => 
+                    board.checkCell(el) !== "dragon" && board.checkCell(el) !== "ant" && board.checkCell(el) !== "food");
+                randomWay = filteredNeighbors[Base.randomNumber(0, filteredNeighbors.length - 1)];
+                break;
+            }
+        }
+        
+        if (randomWay === undefined) return;
+        
         // Reduce HP or kill ant
         if (this.health > 10) {
             this.health -= 10;
@@ -20,16 +43,6 @@ class Ant extends Base {
             board.deleteElement({ x: this.x, y: this.y });
         }
 
-        // If found food, eat it
-        neighbors.forEach((n) => {
-            if (board.checkCell(n) === "food") {
-                randomWay = n;
-                
-                this.health += 20;
-                return;
-            }
-        });
-        
         board.moveToAnotherPlace({ x: this.x, y: this.y }, randomWay);
         
         // Update current ant cords
--- src/gameElements/Dragon.js
+++ src/gameElements/Dragon.js
@@ -2,9 +2,73 @@ import Base from "./Base";
 
 // Dragon character class
 class Dragon extends Base {
-    constructor(health, name) {
-        super(name);
+    constructor(health, name, x, y) {
+        super(name, x, y);
         this.health = health;
+        this.isMoved = false;
+    }
+
+    // Move to next position
+    move(board) {
+        let neighbors;
+        let allNeighbors;
+        let toSubtractNeighbors;
+        let randomWay;
+        
+        allNeighbors = this.getNeighbors(board, 2);
+        toSubtractNeighbors = this.getNeighbors(board, 1);
+
+        // Create moving area for dragon
+        neighbors = allNeighbors.filter((n) => {
+            for (let i = 0; i < toSubtractNeighbors.length; i++) {
+                if (toSubtractNeighbors[i].x === n.x && toSubtractNeighbors[i].y === n.y) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+
+        if (neighbors.length === 0) return;
+        randomWay = neighbors[Base.randomNumber(0, neighbors.length - 1)];
+
+        // Actions controller
+        for (let i = 0; i < neighbors.length; i++) {
+            if (board.checkCell(neighbors[i]) === "ant") {
+                randomWay = neighbors[i];
+
+                if ((this.health + board.getElement(neighbors[i]).health) > 200) this.health = 200;
+                else this.health += board.getElement(neighbors[i]).health;
+                break;
+
+            } else if (board.checkCell(neighbors[i]) === "food" || board.checkCell(neighbors[i]) === "dragon") {
+                let filteredNeighbors = neighbors.filter((el) =>
+                    board.checkCell(el) !== "dragon" && board.checkCell(el) !== "food" && board.checkCell(el) !== "ant");
+                randomWay = filteredNeighbors[Base.randomNumber(0, filteredNeighbors.length - 1)];
+                break;
+            }
+        }
+
+        if (randomWay === undefined) return;
+
+        // Reduce HP or kill dragon
+        if (this.health > 10) {
+            this.health -= 10;
+        } else {
+            board.deleteElement({ x: this.x, y: this.y });
+        }
+
+        board.moveToAnotherPlace({ x: this.x, y: this.y }, randomWay);
+
+        // Update current dragon cords
+        this.x = randomWay.x;
+        this.y = randomWay.y;
+    }
+
+    // Show Dragon stats
+    showStats() {
+        console.log(`Health: ${ this.health }`);
+        console.log(`PositionX: ${ this.x } | PositionY: ${ this.y }`);
     }
 }
 
