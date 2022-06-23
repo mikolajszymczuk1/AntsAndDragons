c0f85cb388e3a5ae3e20313188b4b96c9b9bf531
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-06-23 21:41:36 +0200

    Finished e2e tests for currently app functionality

--- cypress/e2e/boardFunctionality.cy.js
+++ cypress/e2e/boardFunctionality.cy.js
@@ -1,5 +1,41 @@
 describe("Board functionality", () => {
-    it("visits the app root url", () => {
+    it("Go to main page", () => {
         cy.visit("index.html");
+    });
+
+    it("After click on each button, them should render correct game element", () => {
+        cy.viewport(1440, 1028);
+        cy.get("[data-test=control-button]").eq(2).click();
+        
+        cy.get("[data-test=cell]").each(($el) => {
+            cy.wrap($el)
+                .should("contain.text", "");
+        });
+        
+        ["ant", "dragon", "food"].forEach((gameElement) => {
+            cy.get("[data-test=cell]").each(($el) => {
+                cy.wrap($el)
+                    .click()
+                    .get(`[data-test=${ gameElement }]`);
+            });
+        });
+
+        cy.get("[data-test=cell]").each(($el) => {
+            cy.wrap($el)
+                .click()
+                .should("contain.text", "");
+        });
+    });
+
+    it("Round text should shows correct number of round", () => {
+        cy.viewport(1440, 1080);
+        cy.get("[data-test=round-count]").as("rounds")
+            .should("have.text", " Round: 0");
+
+        Array(15).fill(0).forEach(() =>
+            cy.get("[data-test=control-button]").eq(1).click()
+        );
+
+        cy.get("@rounds").should("have.text", " Round: 15");
     });
 });
